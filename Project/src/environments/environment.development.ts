85e57ab416f5b1f92e84aff1ced97f960f17157a
Anzori-PR <saxelashvilianzori@gmail.com>
2025-03-06 13:42:14 +0400

    Deploy

--- Project/src/environments/environment.development.ts
+++ Project/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://192.168.100.4:3001',
-    imageBaseUrl: 'http://192.168.100.4:3001/uploads/company-logos/'
+    apiUrl: 'https://geo-jobs-backend.onrender.com',
+    imageBaseUrl: 'https://geo-jobs-backend.onrender.com/uploads/company-logos/'
   };
